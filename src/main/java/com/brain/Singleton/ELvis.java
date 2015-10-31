e3954e248e568230365c331d20d8a34d5d63f1a3
hubiao <hubiao@ccssoft.com.cn>
2015-10-31 23:27:13 +0800

    延迟的单例

---
+++ src/main/java/com/brain/Singleton/ELvis.java
@@ -0,0 +1,20 @@
+package com.brain.Singleton;
+
+/**
+ * Created by Brainhu on 15/10/31.
+ */
+public class ELvis {
+    private ELvis(){}
+
+    public static final ELvis getInstance(){
+        return ELvisHolder.INSTANCE;
+    }
+
+    private static class ELvisHolder{
+        private static final ELvis INSTANCE = new ELvis();
+    }
+
+    public void leaveTheBuilding(){
+
+    }
+}
